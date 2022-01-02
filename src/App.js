4d7700bbcae27d4ed7f1cace0cb7757a6eade31f
DDH <dung.ho@elca.vn>
2022-01-02 19:20:15 +0700

    Compound components - 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function Toggle({ children }) {
   // return <Switch on={on} onClick={toggle} />;
 
   return React.Children.map(children, (child) => {
-    return React.cloneElement(child, { on, toggle });
+    if (allowTypes.includes(child.type)) {
+      return React.cloneElement(child, { on, toggle });
+    }
+    return child;
   });
 }
 
@@ -22,12 +25,20 @@ function Toggle({ children }) {
 
 // Accepts `on` and `children` props and returns `children` if `on` is true
 const ToggleOn = ({ on, children }) => (on ? children : null);
-
 // Accepts `on` and `children` props and returns `children` if `on` is false
 const ToggleOff = ({ on, children }) => (on ? null : children);
-
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({ on, toggle }) => <Switch on={on} onClick={toggle} />;
+
+const MyToggleButton = ({ on, toggle }) => {
+  return on ? (
+    <button onClick={toggle}>The button is on yo.</button>
+  ) : (
+    <button onClick={toggle}>The button is off soooo.</button>
+  );
+};
+
+const allowTypes = [ToggleOn, ToggleOff, ToggleButton, MyToggleButton];
 
 function App() {
   return (
@@ -35,6 +46,9 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <br />
+        <MyToggleButton />
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
