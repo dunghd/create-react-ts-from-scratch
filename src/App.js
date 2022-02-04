c086cf9f306166a979d942bd73c3c3ad48bbeed1
DDH <dung.ho@elca.vn>
2022-02-04 17:57:48 +0700

    Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
 import React from "react";
-import { Switch } from "./switch";
-import { ToggleProvider, useToggle } from "./context/toggle-context";
-
-const ToggleButton = (props) => {
-  const { on, getTogglerProps } = useToggle();
-  return <Switch {...getTogglerProps({ on })} />;
-};
-
-const CustomButton = (props) => {
-  const { on, getTogglerProps } = useToggle();
-  return (
-    <button
-      {...getTogglerProps({
-        "arial-label": "custom-button",
-        onClick: (e) => console.info("onButtonClick", e),
-      })}
-    >
-      {on ? "on" : "off"}
-    </button>
-  );
-};
 
 function App() {
-  return (
-    <div>
-      <ToggleProvider>
-        <ToggleButton />
-        <hr />
-        <CustomButton />
-      </ToggleProvider>
-    </div>
-  );
+  return <div>DDH</div>;
 }
 
 export default App;
