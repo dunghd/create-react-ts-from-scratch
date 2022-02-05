7473786824c3761952a82be8edc215d2b716e78f
DDH <dung.ho@elca.vn>
2022-02-05 10:50:15 +0700

    Training-HOC-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
 import React from "react";
+import Hoc from "./HOC";
+import StockList from "./StockList";
+import UserList from "./UserList";
 
-function App() {
-  return <div>DDH</div>;
+const stockData = [
+  {
+    id: 1,
+    name: "TCS",
+  },
+  {
+    id: 2,
+    name: "Infosys",
+  },
+  {
+    id: 3,
+    name: "Reliance",
+  },
+];
+
+const usersData = [
+  {
+    id: 1,
+    name: "Krunal",
+  },
+  {
+    id: 2,
+    name: "Ankit",
+  },
+  {
+    id: 3,
+    name: "Rushabh",
+  },
+];
+
+const Stock = Hoc(StockList, stockData);
+
+const User = Hoc(UserList, usersData);
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <span>Stock List</span>
+        <br />
+        <Stock />
+        <br />
+        <span>User List</span>
+        <br />
+        <User />
+      </>
+    );
+  }
 }
+
+App = Hoc(App);
 
 export default App;
